bac93ddab3e67de3cd1b7180c642bcf538a64ea2
Tomasz Drzewiecki <kn65op@gmail.com>
2012-10-28 11:34:31 +0100

    very small part of interfaces

--- BayerFilter/BayerFilter/OpenCLAlgorithm.h
+++ BayerFilter/BayerFilter/OpenCLAlgorithm.h
@@ -1,8 +1,15 @@
 #pragma once
+
+#include "OpenCLDevice.h"
+
 class OpenCLAlgorithm
 {
 public:
   OpenCLAlgorithm(void);
   ~OpenCLAlgorithm(void);
+
+  void setDevice(OpenCLDevice dev);
+public:
+  OpenCLDevice device;
 };
 
--- BayerFilter/BayerFilter/OpenCLDevice.h
+++ BayerFilter/BayerFilter/OpenCLDevice.h
@@ -1,8 +1,15 @@
 #pragma once
+
+#include <list>
+
+#include <opencl.h>
+
 class OpenCLDevice
 {
 public:
   OpenCLDevice(void);
   ~OpenCLDevice(void);
+
+  static std::list<OpenCLDevice> getDevices();
 };
 
