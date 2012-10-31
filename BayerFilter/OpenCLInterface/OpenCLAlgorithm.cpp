2b74eab0c3aef8ca39601cd07ff37b74a2f73e93
Bartek Bulat <bartek.bulat@gmail.com>
2012-10-31 22:15:43 +0100

    OpenCLAlgorithm interface

--- BayerFilter/OpenCLInterface/OpenCLAlgorithm.cpp
+++ BayerFilter/OpenCLInterface/OpenCLAlgorithm.cpp
@@ -7,7 +7,16 @@ OpenCLAlgorithm::OpenCLAlgorithm(void)
   
 }
 
+OpenCLAlgorithm::OpenCLAlgorithm(OpenCLDevice dev)
+{
+  device = dev;
+}
 
 OpenCLAlgorithm::~OpenCLAlgorithm(void)
 {
+}
+
+void OpenCLAlgorithm::setDevice(OpenCLDevice dev)
+{
+  device = dev;
 }
--- BayerFilter/OpenCLInterface/OpenCLAlgorithm.h
+++ BayerFilter/OpenCLInterface/OpenCLAlgorithm.h
@@ -8,14 +8,23 @@ struct OpenCLAlgorithmException : public OpenCLException
   OpenCLAlgorithmException(std::string m, int e) : OpenCLException(m,e) {}
 };
 
+struct OpenCLAlgorithmParams {};
+
 class OpenCLAlgorithm
 {
 public:
   OpenCLAlgorithm(void);
+  OpenCLAlgorithm(OpenCLDevice dev);
   ~OpenCLAlgorithm(void);
 
   void setDevice(OpenCLDevice dev);
-public:
+  void setParams(const OpenCLAlgorithmParams & params) = 0;
+  void prepare() = 0;
+  void run(const uchar * data_input, size_t di_size, uchar * data_output, size_t do_size) = 0;
+  
+private:
   OpenCLDevice device;
+  cl_kernel kernel;
+  cl_program program;  
 };
 
