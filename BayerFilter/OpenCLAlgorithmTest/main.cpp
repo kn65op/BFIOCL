ab2d86de10b400d6616d0eb4620d3bd378c66c39
Tomasz Drzewiecki <kn65op@gmail.com>
2012-11-01 18:30:50 +0100

    Add OpenCLAlgorithmTest

--- BayerFilter/OpenCLAlgorithmTest/main.cpp
+++ BayerFilter/OpenCLAlgorithmTest/main.cpp
@@ -1,5 +1,40 @@
 #include "gtest/gtest.h"
 
+#include <OpenCLAlgorithm.h>
+
+class SetDevice : public ::testing::Test
+{
+protected:
+  void SetUp()
+  {
+    device = OpenCLDevice::getDevices().front();
+  }
+
+  class OpenCLAlgorithmTest : public OpenCLAlgorithm
+  {
+  public:
+    void setParams(const OpenCLAlgorithmParams & params)
+    {
+      throw OpenCLAlgorithmException("Not implemented", 0);
+    }
+    void prepare()
+    {
+      throw OpenCLAlgorithmException("Not implemented", 0);
+    }
+    void run(const char * data_input, size_t di_size, char * data_output, size_t do_size)
+    {
+      throw OpenCLAlgorithmException("Not implemented", 0);
+    }
+  };
+
+  OpenCLDevice device;
+  OpenCLAlgorithmTest al;
+};
+
+TEST_F(SetDevice, SetProperDevice)
+{
+  ASSERT_NO_THROW(al.setDevice(device));
+}
 
 
 int main(int argc, char** argv) 
